4568f34ba49215e63b05198c66715456f313a288
zhangmo8 <wegi866@gmail.com>
2023-07-28 18:58:45 +0800

    feat: Video playback and pause, progress bar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,12 +5,15 @@ defineOptions({
   name: 'IndexPage',
 })
 
+const progressBarWidth = 400
+
 interface VideoType {
   id: string
   url: string
   start: number
   timeRange: number
   isPlayed?: boolean
+  background?: string
 }
 
 // actions
@@ -28,13 +31,15 @@ const videos: VideoType[] = [
     start: 0,
     timeRange: 5,
     isPlayed: false,
+    background: 'rgb(253, 209, 57)',
   },
   {
     id: '2',
     url: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
-    start: 4,
+    start: 1,
     timeRange: 5,
     isPlayed: false,
+    background: 'rgb(29, 206, 113)',
   },
 ]
 
@@ -44,22 +49,25 @@ const endingTime: ComputedRef<number> = computed(() => {
   }))
 })
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++)
-    color += letters[Math.floor(Math.random() * 16)]
+const secondToPixel = computed(() => {
+  return progressBarWidth / endingTime.value
+})
 
-  return color
-}
+const progress = computed<CSSProperties>(() => {
+  return {
+    transform: `translateX(${secondToPixel.value * currentTime.value > progressBarWidth ? progressBarWidth - 4 : secondToPixel.value * currentTime.value}px)`,
+  }
+})
+
+const isEnded: ComputedRef<boolean> = computed(() => {
+  return currentTime.value >= endingTime.value
+})
 
 function generateStyle(v: VideoType): CSSProperties {
-  const atom = 400 / endingTime.value
-  const offsetWidth = atom * v.start
-  const width = v.timeRange * atom
+  const offsetWidth = secondToPixel.value * v.start
+  const width = v.timeRange * secondToPixel.value
 
   return {
-    background: getRandomColor(),
     width: `${width}px`,
     transform: `translateX(${offsetWidth}px)`,
   }
@@ -73,38 +81,58 @@ function onToggle() {
   else onPause()
 }
 
+function onRePlay() {
+  currentTime.value = 0
+  videos.forEach((v, i) => {
+    v.isPlayed = false
+    videoRefs.value[i].currentTime = 0
+  })
+  onPlay()
+}
+
 function onPlay() {
   if (currentTime.value < endingTime.value) {
-    currentTime.value += 1
-
     videos.forEach((video, index) => {
-      if (video && !video.isPlayed && currentTime.value >= video.start) {
+      const videoEnd = video.start + video.timeRange
+
+      if (video && !video.isPlayed && currentTime.value >= video.start && currentTime.value < videoEnd) {
         video.isPlayed = true
         videoRefs.value[index].play()
       }
     })
-
-    // videoRefs.value.forEach((v) => {
-    //   const id = v.getAttribute('id')
-    //   const video = videos.find(v => v.id === id)
-
-    //   if (video && !video.isPlayed && currentTime.value >= video.start) {
-    //     video.isPlayed = true
-    //     v.play()
-    //   }
-    // })
-    timer.value = setTimeout(onPlay, 1000)
+    currentTime.value += 0.25
+    timer.value = setTimeout(onPlay, 250)
   }
 }
 
 function onPause() {
   timer.value && clearTimeout(timer.value)
+
+  let currentIndex = null
+
+  videos.forEach((v, i) => {
+    if (v.isPlayed) {
+      currentIndex = i
+      videoRefs.value[i].pause()
+      v.isPlayed = false
+    }
+  })
+
+  if (currentIndex || currentIndex === 0)
+    currentTime.value = videoRefs.value[currentIndex].currentTime + videos[currentIndex].start
+
   timer.value = null
 }
 
 function setRefs(r: Element | ComponentPublicInstance | null) {
   r && videoRefs.value.push(r as HTMLVideoElement)
 }
+
+// TODO：All video time update will trigger this function Single case. Get the max time and use it to be currentTime
+// const computedTime = useThrottleFn((event: Event, index: number) => {
+//   const videoTime = (event.target as HTMLVideoElement)?.currentTime + videos[index].start
+//   currentTime.value = videoTime
+// }, 100)
 </script>
 
 <template>
@@ -118,12 +146,15 @@ function setRefs(r: Element | ComponentPublicInstance | null) {
       </div>
     </div>
     <div relative box-border h-50px w-400px rd-2 bg-white>
-      <div absolute h-full w-4px bg-teal />
-      <div v-for="item in videos" :key="item.id" m-t-10px h-5px rd-5px :style="generateStyle(item)" />
+      <div absolute left-0 h-full w-4px bg-teal transition-transform :style="progress" />
+      <div v-for="item in videos" :key="item.id" m-t-10px h-5px rd-5px :style="{ ...generateStyle(item), background: item.background }" />
     </div>
 
-    <div m-t-20px cursor-pointer btn @click="onToggle">
+    <div v-if="!isEnded" m-t-20px cursor-pointer btn @click="onToggle">
       {{ isPlay ? '暂停' : '播放' }}{{ currentTime }}
+    </div>
+    <div v-else m-t-20px cursor-pointer btn @click="onRePlay">
+      <div />
     </div>
   </div>
 </template>
